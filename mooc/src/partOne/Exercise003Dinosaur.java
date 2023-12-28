052657db37b752e9fe8fcedc985fcee0239895e2
Mr. Linxed <hello@mrlinxed.com>
2023-12-28 12:55:11 +0100

    Exercise 3

---
+++ mooc/src/partOne/Exercise003Dinosaur.java
@@ -0,0 +1,9 @@
+package partOne;
+
+public class Exercise003Dinosaur {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
+    }
+}

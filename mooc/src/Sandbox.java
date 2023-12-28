804a7d4de16d807305ce5828ed4ae64d25aa203c
Mr. Linxed <hello@mrlinxed.com>
2023-12-28 16:58:23 +0100

    Exercise 10

--- mooc/src/Sandbox.java
+++ mooc/src/Sandbox.java
@@ -23,5 +23,15 @@ public class Sandbox {
         String end = ", James Bond";
 
         System.out.println(start + "Bond" + end);
+
+        String text = "contains text";
+        int wholeNumber = 123;
+        double floatingPoint = 3.141592653;
+        boolean trueOrFalse = true;
+
+        System.out.println("Text variable: " + text);
+        System.out.println("Integer variable: " + wholeNumber);
+        System.out.println("Floating-point variable: " + floatingPoint);
+        System.out.println("Boolean: " + trueOrFalse);
     }
 }

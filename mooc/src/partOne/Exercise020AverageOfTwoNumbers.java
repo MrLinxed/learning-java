871c654434e6983c49e4c2d44ea35044d1126367
Mr. Linxed <hello@mrlinxed.com>
2023-12-29 18:15:27 +0100

    Exercise 20

---
+++ mooc/src/partOne/Exercise020AverageOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package partOne;
+
+import java.util.Scanner;
+
+public class Exercise020AverageOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        double average = ((double) first + second) / 2;
+
+        System.out.println("The average is " + average);
+    }
+}

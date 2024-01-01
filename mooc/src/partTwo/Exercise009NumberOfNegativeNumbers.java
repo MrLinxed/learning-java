be948eb5571101fd4660a12a143224a9d5cb17bd
Mr. Linxed <hello@mrlinxed.com>
2024-01-01 15:13:52 +0100

    Part Two: Exercise 9

---
+++ mooc/src/partTwo/Exercise009NumberOfNegativeNumbers.java
@@ -0,0 +1,27 @@
+package partTwo;
+
+import java.util.Scanner;
+
+public class Exercise009NumberOfNegativeNumbers {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int count = 0;
+
+        while(true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(reader.nextLine());
+
+            if(number == 0) {
+                break;
+            }
+
+            if(number < 0) {
+                count = count + 1;
+            }
+        }
+
+
+        System.out.println("Number of negative numbers: " + count);
+    }
+}

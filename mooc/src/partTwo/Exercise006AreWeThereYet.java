f804d245ea930f57bdd90a40eff1cc7c6b437805
Mr. Linxed <hello@mrlinxed.com>
2024-01-01 14:56:48 +0100

    Part Two: Exercise 6

---
+++ mooc/src/partTwo/Exercise006AreWeThereYet.java
@@ -0,0 +1,17 @@
+package partTwo;
+
+import java.util.Scanner;
+
+public class Exercise006AreWeThereYet {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Give a number:");
+            int number = Integer.parseInt(reader.nextLine());
+            if(number == 4) {
+                break;
+            }
+        }
+    }
+}

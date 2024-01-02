1227af337976bad65020036557ed3d59f986180a
Mr. Linxed <hello@mrlinxed.com>
2024-01-02 20:33:57 +0100

    Part Two: Exercise 14

---
+++ mooc/src/partTwo/Exercise014Counting.java
@@ -0,0 +1,14 @@
+package partTwo;
+
+import java.util.Scanner;
+
+public class Exercise014Counting {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        int countInput = Integer.parseInt(reader.nextLine());
+        for(int i = 0; i <= countInput; i++) {
+            System.out.println(i);
+        }
+    }
+}

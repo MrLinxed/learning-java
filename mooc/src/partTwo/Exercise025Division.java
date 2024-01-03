a92958401b43e6e6c8e71445fb5f7365f247acd8
Mr. Linxed <hello@mrlinxed.com>
2024-01-03 18:35:17 +0100

    Part Two: Exercise 25

---
+++ mooc/src/partTwo/Exercise025Division.java
@@ -0,0 +1,11 @@
+package partTwo;
+
+public class Exercise025Division {
+    public static void main(String[] args) {
+        division(3, 5);
+    }
+
+    public static void division(int numerator, int denominator) {
+        System.out.println((double)numerator / denominator);
+    }
+}

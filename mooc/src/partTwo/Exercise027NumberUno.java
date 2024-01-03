11ec6cb19bcf4f10f0d3c3cc2f5027c53969c0c2
Mr. Linxed <hello@mrlinxed.com>
2024-01-03 18:39:41 +0100

    Part Two: Exercise 27

---
+++ mooc/src/partTwo/Exercise027NumberUno.java
@@ -0,0 +1,11 @@
+package partTwo;
+
+public class Exercise027NumberUno {
+
+    public static void main(String[] args) {
+        System.out.println(numberUno());
+    }
+    public static int numberUno() {
+        return 1;
+    }
+}

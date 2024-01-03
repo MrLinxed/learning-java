5ac94063f48cb931d29183c38f1735ec2fedd784
Mr. Linxed <hello@mrlinxed.com>
2024-01-03 18:40:24 +0100

    Part Two: Exercise 28

---
+++ mooc/src/partTwo/Exercise028Word.java
@@ -0,0 +1,11 @@
+package partTwo;
+
+public class Exercise028Word {
+
+    public static void main(String[] args) {
+        System.out.println(word());
+    }
+    public static String word() {
+        return "word";
+    }
+}

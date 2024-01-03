c5bc16e7099e25cc31aee86467f90f0d9bf30618
Mr. Linxed <hello@mrlinxed.com>
2024-01-03 18:27:27 +0100

    Part Two: Exercise 21

---
+++ mooc/src/partTwo/Exercise021InAHoleInTheGround.java
@@ -0,0 +1,11 @@
+package partTwo;
+
+public class Exercise021InAHoleInTheGround {
+    public static void main(String[] args) {
+        Exercise021InAHoleInTheGround.printText();
+    }
+
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
